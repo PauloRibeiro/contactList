bce46dfcc3be965ba39a34022981161a6a3dfcf7
Paulo Henrique de Oliveira Ribeiro <pribeiro05@prestadores.amil.com.br>
2017-12-08 11:37:31 -0200

    Mock contatos, exibindo na tabela

---
+++ app/contatos/contatos-mock.js
@@ -0,0 +1,34 @@
+"use strict";
+exports.CONTATOS = [
+    {
+        id: 1,
+        nome: 'Juca Pirama',
+        email: 'jucapirama@gmail.com',
+        telefone: '(00) 0000-0000'
+    },
+    {
+        id: 2,
+        nome: 'Gavião Bueno',
+        email: 'bueno@globo.com',
+        telefone: '(00) 0000-0000'
+    },
+    {
+        id: 3,
+        nome: 'Silvio Santos',
+        email: 'silvio-santos@sbt.com.br',
+        telefone: '(00) 0000-0000'
+    },
+    {
+        id: 4,
+        nome: 'Sr Madruga',
+        email: 'madruga@gmail.com',
+        telefone: '(00) 0000-0000'
+    },
+    {
+        id: 5,
+        nome: 'Jorge Tadeu',
+        email: 'cesartadeu@gmail.com',
+        telefone: '(00) 0000-0000'
+    }
+];
+//# sourceMappingURL=contatos-mock.js.map
