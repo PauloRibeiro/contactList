a44ded05af4af99518ad62c9d4a1aa267c0e5aab
Paulo Henrique de Oliveira Ribeiro <pribeiro05@prestadores.amil.com.br>
2017-12-21 15:54:44 -0200

    mensagem de erro no formulário de novo contato

--- app/contatos/contato-detalhe.component.ts
+++ app/contatos/contato-detalhe.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { ContatoService } from './contato.service';
+import { Contato } from './contato.model';
 
 
 @Component({
@@ -6,4 +11,41 @@ import { Component } from '@angular/core'
     selector: 'contato-detalhe',
     templateUrl: 'contato-detalhe.component.html'
 })
-export class ContatoDetalheComponent {}
+export class ContatoDetalheComponent {
+    contato: Contato;
+
+    constructor(
+        private contatoService: ContatoService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {}
+
+    ngOnInit(): void {
+        this.contato = new Contato(0, '', '', '');
+
+        this.route.params.forEach((params: Params) => {
+            let id:number = +params['id'];
+
+            //console.log(id);
+
+            if(id) {
+                this.contatoService.getContato(id)
+                .then((contato: Contato) => {
+                    this.contato = contato;
+                });
+            }
+        });
+    }
+
+    getFormControlClass(isValid: boolean, isPristine: boolean): {} {
+        return {
+            'form-control': true,
+            'is-invalid': !isValid && !isPristine,
+            'is-valid': isValid && !isPristine
+        };
+    }
+
+    teste():void {
+        console.log('teste');
+    }
+}
--- app/contatos/contatos-lista.component.ts
+++ app/contatos/contatos-lista.component.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { Contato } from "./contato.model";
-import { CONTATOS } from "./contatos-mock";
+import { ContatoService } from "./contato.service";
 
 
 @Component({
@@ -9,6 +9,15 @@ import { CONTATOS } from "./contatos-mock";
     selector: 'contatos-lista',
     templateUrl: 'contatos-lista.component.html'
 })
-export class ContatosListaComponent {
-    contatos: Contato[] = CONTATOS;
+export class ContatosListaComponent implements OnInit {
+    contatos: Contato[];
+
+    constructor(private contatoService: ContatoService) {}
+
+    ngOnInit(): void {
+        this.contatoService.getContatos()
+            .then((contatos: Contato[]) => {
+                this.contatos = contatos;
+            }).catch(err => console.log(err));
+    }
 }
